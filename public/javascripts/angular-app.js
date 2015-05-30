32604e95ce47ada5b07054dc0db177b9fcac5018
Patrick Racenberg <pracenberg@gmail.com>
2015-05-30 16:40:22 -0700

    added a templates folder for partials. included nav and footer templates

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -1,8 +1,13 @@
 'use strict'
 
-var homeRoom = angular.module('homeRoom', []);
+var homeRoom = angular.module('homeRoom', ['ui.router']);
 
 homeRoom.controller('mainCtrl', ['$scope', function($scope) {
   $scope.welcome = 'Hi this is the main page';
+}]);
+
+homeRoom.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider,   $urlRouterProvider) { 
+  
+
 }]);
 
