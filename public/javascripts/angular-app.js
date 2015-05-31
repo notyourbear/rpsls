3d0857f83c07748db3fdafe904213b0a458ff120
Patrick Racenberg <pracenberg@gmail.com>
2015-05-30 23:11:01 -0700

    input box now hides when username added. additional validation check made

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -5,11 +5,12 @@ var app = angular.module('homeRoom', ['ui.router']);
 app.controller('mainCtrl', ['$scope', 'socket', function($scope, socket) {
   $scope.welcome = 'Hi this is the main page';
   $scope.addName = function(){
-    if($scope.userName){
+    if($scope.userName && !$scope.named){
       var user = $scope.userName;
       console.log($scope.userName);
       socket.emit('addName', user);
       $scope.userName = '';
+      $scope.named = true;
     }
   };
 }]);
