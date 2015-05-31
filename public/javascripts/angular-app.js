d968b44edb9fb88dcb8b6be732177d8190c58b03
Patrick Racenberg <pracenberg@gmail.com>
2015-05-30 17:08:44 -0700

    added a room controller. added routing for room to test

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -1,13 +1,32 @@
 'use strict'
 
-var homeRoom = angular.module('homeRoom', ['ui.router']);
+var app = angular.module('homeRoom', ['ui.router']);
 
-homeRoom.controller('mainCtrl', ['$scope', function($scope) {
+app.controller('mainCtrl', ['$scope', function($scope) {
   $scope.welcome = 'Hi this is the main page';
 }]);
 
-homeRoom.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider,   $urlRouterProvider) { 
-  
+app.controller('roomCtrl', ['$scope', function($scope) {
+  $scope.welcome = 'Hi this is the room page';
+}]);
+
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider,   $urlRouterProvider) {
+
+  // 'if typed in url is not used, go to homepage'
+  $urlRouterProvider.otherwise('/');
+
+  // use stateProvider for other routings
+  $stateProvider
+    .state('home', {
+      url: '/',
+      controller: 'mainCtrl'
+    })
+    .state('room', {
+      url: '/room',
+      controller: 'roomCtrl'
+    });
+
+
 
 }]);
 
