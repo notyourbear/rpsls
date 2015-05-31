25543c3ec65b9d904dc0e07caceb7dc4e252c4f9
Patrick Racenberg <pracenberg@gmail.com>
2015-05-30 21:32:51 -0700

    moved socket factory into own file for seperation of concerns

--- public/javascripts/angular-app.js
+++ public/javascripts/angular-app.js
@@ -30,29 +30,5 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider,   
 
 }]);
 
-app.factory('socket', function ($rootScope) {
-  var socket = io.connect();
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      });
-    }
-  };
-});
-
 
 
