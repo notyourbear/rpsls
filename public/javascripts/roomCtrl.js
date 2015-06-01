c73f2d9b972697a751bbc03c9dcca27cfba56321
Patrick Racenberg <pracenberg@gmail.com>
2015-06-01 16:50:33 -0700

    test chat workz

--- public/javascripts/roomCtrl.js
+++ public/javascripts/roomCtrl.js
@@ -3,6 +3,14 @@ app.controller('roomCtrl', ['$scope', '$state', 'socket', 'profile', function($s
   $scope.welcome = 'Hi this is the room page';
   $scope.caviat = profile.currentRoomId;
 
+  $scope.chatMsg = function(){
+    socket.emit('chatMessage', $('#m').val());
+        $('#m').val('');
+        return false;
+  };
 
-  
+  socket.on('chatMessage', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+
 }]);
