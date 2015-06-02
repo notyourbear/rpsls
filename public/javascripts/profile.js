20f80ebbe6a589365711f83cf31ce72ff7f9e822
Patrick Racenberg <pracenberg@gmail.com>
2015-06-02 15:14:47 -0700

    frontend also properly keeps track now

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -9,13 +9,24 @@ app.factory('profile', function (){
         hasName: false,
         blah: 'mow',
         inGame: false,
-        hi: 'mew',
         players: [],
         countPlayers: function(gameObject){
+            var isAPlayer;
+
             //run through gameObject
             for (var i in gameObject) {
                 if (gameObject.hasOwnProperty(i)){
-                    this.players.push(i);
+                    //check to see if guy is already in the game: 
+                    for (var j = 0; j < this.players.length; j++){
+                        if(i === this.players[j]) {
+                            isAPlayer = true;
+                        }
+                    }
+
+                    //only push in if not already a player
+                    if (!isAPlayer){
+                        this.players.push(i);
+                    }
                 }
             }
         }
