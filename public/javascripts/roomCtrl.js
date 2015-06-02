3886ef00fc87d016830d4760e8da956fa8562273
Patrick Racenberg <pracenberg@gmail.com>
2015-06-01 23:37:46 -0700

    chatwindow scrolls with new messages

--- public/javascripts/roomCtrl.js
+++ public/javascripts/roomCtrl.js
@@ -12,6 +12,11 @@ app.controller('roomCtrl', ['$scope', '$state', 'socket', 'profile', function($s
       //append message to own screen
       angular.element('#messages').append($('<li>').text(profile.userName+": "+$scope.chatMessage));
 
+      //if message goes past overflow, make sure it's scrolled to.
+      angular.element('#messageSpace').stop().animate({
+        scrollTop: angular.element("#messageSpace")[0].scrollHeight
+      }, 800);
+
       //reset chatMessage text to nothing
       $scope.chatMessage = '';
     }
@@ -19,6 +24,11 @@ app.controller('roomCtrl', ['$scope', '$state', 'socket', 'profile', function($s
 
   socket.on('chatMessage', function(user, msg){
     angular.element('#messages').append($('<li>').text(user+": "+msg));
+
+     //if message goes past overflow, make sure it's scrolled to.
+      angular.element('#messageSpace').stop().animate({
+        scrollTop: angular.element("#messageSpace")[0].scrollHeight
+      }, 800);
   });
 
 }]);
