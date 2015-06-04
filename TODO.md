313dad3640f473c35a46c69d07ec5bc97bd25a46
Patrick Racenberg <pracenberg@gmail.com>
2015-06-04 15:03:39 -0700

    ngRepeat displays room names coorectly after room creation

--- TODO.md
+++ TODO.md
@@ -6,10 +6,12 @@ figure out why adding/removing a person isn't working correctly.
  
 
 issues: 
-  1. leaving a room displays the leave message twice.. barf
   2. rooms being displayed multiple times on home screen 
   2c. also add a join message
 
 
 ...on room delete, send out message to update rooms...
+
+
+//rewrite create room logic to make use of ng-repeat
 
--- public/javascripts/mainCtrl.js
+++ public/javascripts/mainCtrl.js
@@ -41,60 +41,76 @@ app.controller('mainCtrl', ['$scope', '$rootScope', '$compile', '$state', 'socke
     $scope.createRoom = true;
   };
 
-  $scope.addRoom = function(user, name, id){
-    
-    //check if user is creating room
-    if ($scope.roomName !== ''){
-      roomDiv.room.createdBy = profile.userName;
-      roomDiv.room.name = $scope.roomName;
-      $scope.roomName = "";
-    
-    //else assume content provided via args
-    } else {
-      roomDiv.room.createdBy = user;
-      roomDiv.room.name = name;
-    }
+  $scope.addRoom = function(){
+    //make sure created rooms have a name:
+    var name = $scope.roomName || 'Room';
 
-    //create html for room element to add
-    var joinButton = roomDiv.join.buttonBeg + "\""+id+"\"" + roomDiv.join.buttonBegEnd + roomDiv.join.buttonText + roomDiv.join.buttonEnd;
+    //set profile.inGame to true;
+    profile.inGame = true;
 
-    //further creation. also compile the fucker into an angular template
-    var newRoom = $compile(roomDiv.room.roomBeg + roomDiv.room.nameBeg + roomDiv.room.name + roomDiv.room.nameEnd + roomDiv.room.createdBeg + roomDiv.room.createdBy + roomDiv.room.createdEnd + joinButton + roomDiv.room.roomEnd)($scope);
+    //push player to the profile.players
+    profile.players.push(profile.userName);
+    
+    //emit to backEnd that room should be created
+    socket.emit('addRoom', name);
+     
+    
+
+  // $scope.addRoom = function(user, name, id){
+    //check that roomName isn't blank
+    
+    // //check if user is creating room
+    // if ($scope.roomName !== ''){
+    //   roomDiv.room.createdBy = profile.userName;
+    //   roomDiv.room.name = $scope.roomName;
+    //   $scope.roomName = "";
+    
+    // //else assume content provided via args
+    // } else {
+    //   roomDiv.room.createdBy = user;
+    //   roomDiv.room.name = name;
+    // }
+
+    // //create html for room element to add
+    // var joinButton = roomDiv.join.buttonBeg + "\""+id+"\"" + roomDiv.join.buttonBegEnd + roomDiv.join.buttonText + roomDiv.join.buttonEnd;
+
+    // //further creation. also compile the fucker into an angular template
+    // var newRoom = $compile(roomDiv.room.roomBeg + roomDiv.room.nameBeg + roomDiv.room.name + roomDiv.room.nameEnd + roomDiv.room.createdBeg + roomDiv.room.createdBy + roomDiv.room.createdEnd + joinButton + roomDiv.room.roomEnd)($scope);
 
   
-    //validation time!  
-    //check if room creation is being sent from another user
-    if(arguments.length > 0){
-      //if not, create room
-      angular.element('#rooms').append(newRoom);
+    // //validation time!  
+    // //check if room creation is being sent from another user
+    // if(arguments.length > 0){
+    //   //if not, create room
+    //   angular.element('#rooms').append(newRoom);
 
-      //update profile with available rooms
-      profile.availableRooms.push([user, name, id]);
+    //   //update profile with available rooms
+    //   profile.availableRooms.push([user, name, id]);
       
-      //else current user is trying to add a room.
-      //check if user has already created a room and if user has a name
-    } else if (profile.userName && !hasCreatedRoom){
-        angular.element('#rooms').append(newRoom);
+    //   //else current user is trying to add a room.
+    //   //check if user has already created a room and if user has a name
+    // } else if (profile.userName && !hasCreatedRoom){
+    //     angular.element('#rooms').append(newRoom);
 
-        //update profile with available rooms
-        profile.availableRooms.push([user, name, id]);
+    //     //update profile with available rooms
+    //     profile.availableRooms.push([user, name, id]);
 
-      //if user is creating room, make it so he/she cannot create another
-        hasCreatedRoom = true;
+    //   //if user is creating room, make it so he/she cannot create another
+    //     hasCreatedRoom = true;
 
-      //emit room creation to backend
-        socket.emit('addRoom', roomDiv.room.name);
+    //   //emit room creation to backend
+    //     socket.emit('addRoom', roomDiv.room.name);
 
-      //make the create room button dissapear
-        angular.element('#addRooms').hide(300);
+    //   //make the create room button dissapear
+    //     angular.element('#addRooms').hide(300);
 
-      //set profile.inGame to true;
-        profile.inGame = true;
+    //   //set profile.inGame to true;
+    //     profile.inGame = true;
 
-      //push player to the profile.players
-        profile.players.push(profile.userName);
+    //   //push player to the profile.players
+    //     profile.players.push(profile.userName);
 
-    }
+    // }
    
   };
 
@@ -106,25 +122,14 @@ app.controller('mainCtrl', ['$scope', '$rootScope', '$compile', '$state', 'socke
   });
 
   socket.on('currentRooms', function(availableRooms){
-    if(!$scope.bool){
       //console log out available rooms for testing
       console.log(availableRooms);
 
       //display current rooms. since this happens on connection, the server will keep trying to run this over and over. so create/set a bool that will make sure it can only run once
       var i;
 
+      $scope.allTheRooms = availableRooms;
     
-      for (i in availableRooms){
-        if(availableRooms.hasOwnProperty(i)){
-          //add each room
-          $scope.addRoom(availableRooms[i].ownerUserName, availableRooms[i].name, availableRooms[i].id);
-        }
-      }
-    
-    
-      //set bool to true. will only run once now thanks to if check above
-      $scope.bool = true;
-    }
   });
 
   socket.on('checkRoom', function(id, inRoom, players){
